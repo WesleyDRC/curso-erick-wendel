600caee14b46d705c701134e55fce13c3b1f124a
Wesley <wesleymiranda04b@gmail.com>
2021-11-10 19:14:03 -0300

    Manipulando Arquivos - CREATE
    Manipulando Arquivos - CREATE
    continuação do módulo 4

--- 6-cli/test.js
+++ 6-cli/test.js
@@ -12,6 +12,11 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe('Suite de manipulação de Heróis', () => {
+
+    before(async()=>{
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+    
     it('Deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const [resultado] = await database.listar(expected.id)
@@ -19,9 +24,11 @@ describe('Suite de manipulação de Heróis', () => {
     })
 
 
-    // it('deve cadastrar um herói usando arquivos', async ()=>{
-    //     const expected = DEFAULT_ITEM_CADASTRAR
-    //     ok(null, expected)
-    // })   
+    it('deve cadastrar um herói usando arquivos', async ()=>{
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(actual, expected)
+    })   
 
 })
